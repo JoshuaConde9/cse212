9fd5fa64df383f706c76f01e587585560476019f
JoshuaConde9 <joshuaconde0909@gmail.com>
2026-03-11 22:24:49 -0500

    Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,13 +8,28 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        /// TODO Problem 1 Start
+        /// Remember: Using comments in your program, write down your process for solving this problem
+        /// step by step before you write the code. The plan should be clear enough that it could
+        /// be implemented by another person.
 
-        return []; // replace this return statement with your own
+        /// PLAN:
+        /// 1- Make an array that can store the length number
+        /// 2- Loop through each position in the array
+        /// 3- Multiply the number by 1, 2, 3...
+        /// 4- Save each result in the array
+        /// 5- When the loop finishes, return the array
+        /// 
+        double[] result = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+
+        return result;
     }
+
 
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -25,9 +40,22 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        /// TODO Problem 2 Start
+        /// Remember: Using comments in your program, write down your process for solving this problem
+        /// step by step before you write the code. The plan should be clear enough that it could
+        /// be implemented by another person.
+
+        /// PLAN:
+        /// 1- Determine the starting index of the last elements that will move to the front
+        ///    This will be the data.Count - amount
+        /// 2- Use GetRange to take the last 'amount' elements
+        /// 3- Remove those elements from the end of the list
+        /// 4- Insert them at the beginning of the list so it rotates to the right
+
+        int startIndex = data.Count - amount;
+
+        var slice = data.GetRange(startIndex, amount);
+        data.RemoveRange(startIndex, amount);
+        data.InsertRange(0, slice);
     }
 }
